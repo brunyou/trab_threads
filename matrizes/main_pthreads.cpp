a109f2e2f5b1d4614bfb54c6f0c1bd58bc8eb1d1
Bruno Pereira <bruxeetos@gmail.com>
2022-04-03 21:48:05 -0300

    a

--- matrizes/main_pthreads.cpp
+++ matrizes/main_pthreads.cpp
@@ -22,8 +22,8 @@ int j; //colunas de B
 int k; //para  cada linha em A, percorer as células
 
 
-void * print_mat_a(void *arg){
-    int i;
+void *print_mat_a(void * arg){
+    cout << "Executando thread 1... \n";
     cout << "Imprimindo a Matriz A: \n";
     for (int i = 0; i < 3; i++){
         cout << "|";
@@ -32,9 +32,11 @@ void * print_mat_a(void *arg){
             }
         cout << "\n";
     }
+    pthread_exit(NULL);
+    return NULL;
 }
 void * print_mat_b(void *arg){
-    int i;
+    cout << "Executando thread 2... \n";
     cout << "Imprimindo a Matriz B: \n";
     for (int i = 0; i < 3; i++){
         cout << "|";
@@ -43,9 +45,10 @@ void * print_mat_b(void *arg){
             }
         cout << "\n";
     }
+    pthread_exit(NULL);
+    return NULL;
 }
-void * print_mat_c(void *arg){
-    int i;
+void print_mat_c(){
     cout << "O resultado da multiplicação de A x B foi: \n";
     for (int i = 0; i < 3; i++){
         cout << "|";
@@ -57,6 +60,7 @@ void * print_mat_c(void *arg){
 }
 
 void * multiplica_matriz(void *arg){
+    cout << "Executando thread 3... \n";
     for(i = 0; i < 3; i ++){
         for(j = 0; j < 3; j ++){
             for(k = 0; k < 3; k++){
@@ -64,19 +68,21 @@ void * multiplica_matriz(void *arg){
             }
         }
     }
+    cout << "Thread 3 concluida, matrizes multiplicadas... \n";
+    pthread_exit(NULL);
+    return NULL;
 }
 int main(){
 time1 = (double) clock();
 time1 = time1 / CLOCKS_PER_SEC;
 
-pthread_t t1, t2, t3, t4;
-int a1 = 1, a2 = 2, a3 = 3, a4 = 4;
+pthread_t t1, t2, t3;
+int a1 = 1, a2 = 2, a3 = 3;
 
 
-pthread_create(&t1, NULL, print_mat_a, (void *)(&a1)); //cria thread 1
+pthread_create(&t1, NULL, print_mat_a, (void *)(&a1));; //cria thread 1
 pthread_create(&t2, NULL, print_mat_b, (void *)(&a2)); //cria thread 2
 pthread_create(&t3, NULL, multiplica_matriz, (void *)(&a3)); //cria thread 3
-pthread_create(&t4, NULL, print_mat_b, (void *)(&a2)); //cria thread 4
 
 
 
@@ -85,9 +91,9 @@ setlocale(LC_ALL, "Portuguese");
 pthread_join(t1,NULL);
 pthread_join(t2,NULL);
 pthread_join(t3,NULL);
-pthread_join(t4,NULL);
 
 
+print_mat_c();
 
 
 
@@ -96,5 +102,5 @@ timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
     printf("Tempo executado: %f segundos\n", timedif);
 
     pthread_exit(NULL);
-return 0;
+exit (0);
 }
---
+++ vetores/vetores.cpp
@@ -0,0 +1,32 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+#define TAM 100
+
+double time1, timedif;
+
+int array[TAM];
+
+void load (){
+    for(int i = 0; i< TAM; i++){
+        array[i] = i;
+    }  
+}
+
+int main(){
+
+    time1 = (double) clock();
+        time1 = time1 / CLOCKS_PER_SEC;
+
+    load();
+    
+    cout << " Vetor carregado sequencialmente";
+    timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
+            printf("\nTempo executado: %f segundos em single thread", timedif);
+    return 0;
+}
